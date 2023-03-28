02b8bb22c869c3db8b75bdf560302b4bca8a023d
AkbarR1184 <rahmati_ziveh@fzp.czu.cz>
2023-03-28 16:21:20 +0000

    minor change

--- code/source/global_variables.R
+++ code/source/global_variables.R
@@ -11,6 +11,6 @@ library(RColorBrewer)
 library(sf)
 
 PATH_OUTFILES <- "~/shared/data_projects/med_datasets/1000_2000_data" # patch for the Aridification for the past millennium 
-STUDY_PERIOD_START <- as.Date("1000-04-01")
-STUDY_PERIOD_END <- as.Date("2000-04-01")
+STUDY_PERIOD_START <- as.Date("1000-01-01")
+STUDY_PERIOD_END <- as.Date("2000-01-01")
 STUDY_AREA <- extent(-10.25, 40.25, 29.75, 45.25) # Extent for Mediterranean region
