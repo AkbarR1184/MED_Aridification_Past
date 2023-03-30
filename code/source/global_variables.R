25114188b27d890b6a8cd2cf2cee6cff8bc2951d
AkbarR1184 <rahmati_ziveh@fzp.czu.cz>
2023-03-30 08:08:20 +0000

    01_crop_data.R functions and minor changes in global_variables.R file

--- code/source/global_variables.R
+++ code/source/global_variables.R
@@ -9,8 +9,8 @@ library(ggplot2)
 library(data.table)
 library(RColorBrewer)
 library(sf)
-
-PATH_OUTFILES <- "~/shared/data_projects/med_datasets/1000_2000_data" # patch for the Aridification for the past millennium 
-STUDY_PERIOD_START <- as.Date("1000-04-01")
-STUDY_PERIOD_END <- as.Date("2000-04-01")
-STUDY_AREA <- extent(-10.25, 40.25, 29.75, 45.25) # Extent for Mediterranean region
+PATH_INFILES <- "~/shared/data/paleo/PHYDA" # Path for the input files 
+PATH_OUTFILES <- "~/shared/data_projects/med_datasets/1000_2000_data/" # Path for the cropped files 
+PERIOD_START <- as.Date("1000-04-01")
+PERIOD_END <- as.Date("2000-04-01")
+study_area <- extent(-10.25, 40.25, 29.75, 45.25) # Extent for Mediterranean region
