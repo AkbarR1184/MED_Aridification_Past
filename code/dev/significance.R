7c2637e8009ef8fbdb29f25258803a031be21de2
AkbarR1184 <rahmati_ziveh@fzp.czu.cz>
2023-04-09 15:05:11 +0000

    changing from percentile 10 & 90 to percentile 20 and 80

--- code/dev/significance.R
+++ code/dev/significance.R
@@ -13,47 +13,47 @@ tas_slope<-PHYDA[Variable=="tas_mn"]
 
 #calculating p10 and p90 for the PDSI
 
-pdsi_slope[, c("Positive_Trend_P10", "Positive_Trend_P90", "Negative_Trend_P10", "Negative_Trend_P90") :=
-                         .(quantile(Slope[Slope>=0], 0.1, na.rm = TRUE),
-                           quantile(Slope[Slope>=0], 0.9, na.rm = TRUE),
-                           quantile(Slope[Slope<0], 0.1, na.rm = TRUE),
-                           quantile(Slope[Slope<0], 0.9, na.rm = TRUE)), by = Interval]
+pdsi_slope[, c("Positive_Trend_P20", "Positive_Trend_P80", "Negative_Trend_P20", "Negative_Trend_P80") :=
+                         .(quantile(Slope[Slope>=0], 0.2, na.rm = TRUE),
+                           quantile(Slope[Slope>=0], 0.8, na.rm = TRUE),
+                           quantile(Slope[Slope<0], 0.2, na.rm = TRUE),
+                           quantile(Slope[Slope<0], 0.8, na.rm = TRUE)), by = Interval]
 
 #setting threshold for the significant upward/downward change based on percentiles
 
 pdsi_slope[, c("sig_positive_slope", "sig_negative_slope") :=
-                         .(ifelse(Slope > Positive_Trend_P90 , "significant Upward change", NA),
-                           ifelse(Slope <  Negative_Trend_P10 , "significant Downward change", NA))]
+                         .(ifelse(Slope > Positive_Trend_P80 , "significant Upward change", NA),
+                           ifelse(Slope <  Negative_Trend_P20 , "significant Downward change", NA))]
 
 
 #calculating p10 and p90 for the SPEI
 
-spei_slope[, c("Positive_Trend_P10", "Positive_Trend_P90", "Negative_Trend_P10", "Negative_Trend_P90") :=
-             .(quantile(Slope[Slope>=0], 0.1, na.rm = TRUE),
-               quantile(Slope[Slope>=0], 0.9, na.rm = TRUE),
-               quantile(Slope[Slope<0], 0.1, na.rm = TRUE),
-               quantile(Slope[Slope<0], 0.9, na.rm = TRUE)), by = Interval]
+spei_slope[, c("Positive_Trend_P20", "Positive_Trend_P80", "Negative_Trend_P20", "Negative_Trend_P80") :=
+             .(quantile(Slope[Slope>=0], 0.2, na.rm = TRUE),
+               quantile(Slope[Slope>=0], 0.8, na.rm = TRUE),
+               quantile(Slope[Slope<0], 0.2, na.rm = TRUE),
+               quantile(Slope[Slope<0], 0.8, na.rm = TRUE)), by = Interval]
 
 #setting threshold for the significant upward/downward change based on percentiles
 
 spei_slope[, c("sig_positive_slope", "sig_negative_slope") :=
-             .(ifelse(Slope > Positive_Trend_P90 , "significant Upward change", NA),
-               ifelse(Slope <  Negative_Trend_P10 , "significant Downward change", NA))]
+             .(ifelse(Slope > Positive_Trend_P80 , "significant Upward change", NA),
+               ifelse(Slope <  Negative_Trend_P20 , "significant Downward change", NA))]
 
 
 #calculating p10 and p90 for the tas
 
-tas_slope[, c("Positive_Trend_P10", "Positive_Trend_P90", "Negative_Trend_P10", "Negative_Trend_P90") :=
-             .(quantile(Slope[Slope>=0], 0.1, na.rm = TRUE),
-               quantile(Slope[Slope>=0], 0.9, na.rm = TRUE),
-               quantile(Slope[Slope<0], 0.1, na.rm = TRUE),
-               quantile(Slope[Slope<0], 0.9, na.rm = TRUE)), by = Interval]
+tas_slope[, c("Positive_Trend_P20", "Positive_Trend_P80", "Negative_Trend_P20", "Negative_Trend_P80") :=
+             .(quantile(Slope[Slope>=0], 0.2, na.rm = TRUE),
+               quantile(Slope[Slope>=0], 0.8, na.rm = TRUE),
+               quantile(Slope[Slope<0], 0.2, na.rm = TRUE),
+               quantile(Slope[Slope<0], 0.8, na.rm = TRUE)), by = Interval]
 
 #setting threshold for the significant upward/downward change based on percentiles
 
 tas_slope[, c("sig_positive_slope", "sig_negative_slope") :=
-             .(ifelse(Slope > Positive_Trend_P90 , "significant Upward change", NA),
-               ifelse(Slope <  Negative_Trend_P10 , "significant Downward change", NA))]
+             .(ifelse(Slope > Positive_Trend_P80 , "significant Upward change", NA),
+               ifelse(Slope <  Negative_Trend_P20 , "significant Downward change", NA))]
 
 
 
