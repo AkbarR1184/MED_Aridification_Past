ac490f8998bda92446b54ed61b5e7bc4e1581ed0
AkbarR1184 <rahmati_ziveh@fzp.czu.cz>
2023-04-16 12:08:35 +0000

    slope anlysis from 1010 to 2000

--- code/04_slope_analysis.R
+++ code/04_slope_analysis.R
@@ -5,21 +5,22 @@ source("code/source/functions.R")
 
 PHYDA <- readRDS(paste0(PATH_OUTFILES, "PHYDA_pdsi_spei_tas_dt_1000_2000_yearly_0.25_degree.rds"))
 PHYDA$year <- year(PHYDA$time)
+PHYDA_1010_2000<- PHYDA[PHYDA$year > 1009, ]
 PHYDA$time=NULL
 
 #subsetting variables
-pdsi_dt<-PHYDA[variable=="pdsi_mn"]
-spei_dt<-PHYDA[variable=="spei_mn"]
-tas_dt<-PHYDA[variable=="tas_mn"]
+pdsi_dt<-PHYDA_1010_2000[variable=="pdsi_mn"]
+spei_dt<-PHYDA_1010_2000[variable=="spei_mn"]
+tas_dt<-PHYDA_1010_2000[variable=="tas_mn"]
 
-year_interval <- seq(min(pdsi_dt$year), max(pdsi_dt$year)+30, by=30)
+year_interval <- seq(min(pdsi_dt$year), max(pdsi_dt$year), by=30)
 pdsi_dt$timestep <- NA
 pdsi_dt$timestep=as.character(pdsi_dt$timestep)
 
-#adding timewindow (e.g 1000-1030) to the variable 
+#adding time window (e.g 1000-1030) to the variable pdsi 
 
-for (i in 2:length(year_interval)) {
-  pdsi_dt[year<year_interval[i] & year>=year_interval[i-1], timestep:= paste0(year_interval[i-1], sep="-",year_interval[i])]
+for (i in 1:length(year_interval)) {
+  pdsi_dt[year<=year_interval[i] & year>=year_interval[i-1], timestep:= paste0(year_interval[i-1], sep="-",year_interval[i])]
 }
 
 #calculating slope 
@@ -28,3 +29,37 @@ pdsi_dt_slopes <- pdsi_dt[, {
   linear_model <- lm(value ~ year); coef(linear_model)[2]
 }, by = .(X, Y, timestep)]
 setnames(pdsi_dt_slopes, "V1", "slope")
+
+
+
+
+#adding time window (e.g 1000-1030) to the variable spei 
+
+for (i in 1:length(year_interval)) {
+  spei_dt[year<=year_interval[i] & year>=year_interval[i-1], timestep:= paste0(year_interval[i-1], sep="-",year_interval[i])]
+}
+
+#calculating slope 
+
+spei_dt_slopes <- spei_dt[, {
+  linear_model <- lm(value ~ year); coef(linear_model)[2]
+}, by = .(X, Y, timestep)]
+setnames(spei_dt_slopes, "V1", "slope")
+
+
+
+#adding time window (e.g 1000-1030) to the variable tas 
+
+for (i in 1:length(year_interval)) {
+  tas_dt[year<=year_interval[i] & year>=year_interval[i-1], timestep:= paste0(year_interval[i-1], sep="-",year_interval[i])]
+}
+
+#calculating slope 
+
+tas_dt_slopes <- tas_dt[, {
+  linear_model <- lm(value ~ year); coef(linear_model)[2]
+}, by = .(X, Y, timestep)]
+setnames(tas_dt_slopes, "V1", "slope")
+
+slope_dt<-rbind(pdsi_dt_slopes, spei_dt_slopes, tas_dt_slopes)
+saveRDS(slope_dt, "slope_pdsi_spei_tas_1010-2000.rds")
