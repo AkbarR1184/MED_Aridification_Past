9b78952a1bf888e187a13f5350ce7a5b2a14fda1
AkbarR1184 <rahmati_ziveh@fzp.czu.cz>
2023-04-17 15:09:51 +0000

    calculating frequency change for the whole period except for 1990-2000

--- code/dev/Calculating_Relative_Frequency.R
+++ code/dev/Calculating_Relative_Frequency.R
@@ -3,68 +3,55 @@ source("code/source/functions.R")
 source("code/source/global_variables.R")
 #reading the data and subsetting for frequency plot
 
-slope_30year<-readRDS("~/shared/data_projects/med_datasets/1000_2000_data/slope_pdsi_spei_tas.rds")
-slope_30year[,Interval:=paste0(StartYear, sep="-", EndYear)]
-slope_30year[,TrendSign:= sign(Slope)]
-slope_30year[,c("StartYear", "EndYear")]=NULL
-pdsi_slope_30_year<-slope_30year[Variable=="pdsi_mn"]
-spei_slope_30_year<-slope_30year[Variable=="spei_mn"]
-tas_slope_30_year<-slope_30year[Variable=="tas_mn"]
+slope_30year<-readRDS("~/shared/data_projects/med_datasets/1000_2000_data/slope_pdsi_spei_tas_1000-2000.rds")
+slope_30year[,TrendSign:= sign(slope)]
+slope_30year <- slope_30year[slope_30year$timestep != "1990-2020", ]
+
+pdsi_slope_30_year<-slope_30year[variable=="pdsi_mn"]
+spei_slope_30_year<-slope_30year[variable=="spei_mn"]
+tas_slope_30_year<-slope_30year[variable=="tas_mn"]
 
 #calculating frequency of trend change during 34 time intervals
 
-pdsi_trend_changes <- pdsi_slope_30_year[, .("Frequency (%)" = sum(diff(TrendSign) != 0) / 34 * 100), by = .(X, Y)]
-spei_trend_changes <- spei_slope_30_year[, .("Frequency (%)" = sum(diff(TrendSign) != 0) / 34 * 100), by = .(X, Y)]
-tas_trend_changes <- tas_slope_30_year[, .("Frequency (%)" = sum(diff(TrendSign) != 0) / 34 * 100), by = .(X, Y)]
+pdsi_trend_changes <- pdsi_slope_30_year[, .("Frequency (%)" = sum(diff(TrendSign) != 0) / 33 * 100), by = .(X, Y)]
+spei_trend_changes <- spei_slope_30_year[, .("Frequency (%)" = sum(diff(TrendSign) != 0) / 33 * 100), by = .(X, Y)]
+tas_trend_changes <- tas_slope_30_year[, .("Frequency (%)" = sum(diff(TrendSign) != 0) / 33 * 100), by = .(X, Y)]
+
+#creating trend change data table for all variables
+tas_trend_changes$variable<- "tas_mn"
+pdsi_trend_changes$variable<- "pdsi_mn"
+spei_trend_changes$variable<-"spei_mn"
+
+trendchange_dt<- rbind(pdsi_trend_changes, spei_trend_changes, tas_trend_changes)
+saveRDS(trendchange_dt, "trendchange.rds")
 
 #plotting the frequecny maps
 
-ggplot(data = pdsi_trend_changes, aes(X, Y)) +
-  geom_tile(aes(fill =`Frequency (%)`, color=`Frequency (%)`)) +
-  scale_fill_gradient2(low = "blue",midpoint = 50,mid = "gray",   high = "red") +
-  scale_color_gradient2(low = "blue",midpoint =  50,mid = "gray",   high = "red") +
-  scale_x_continuous(breaks = seq(-20, 30, 10), name = "Longitude") +
-  scale_y_continuous(breaks = seq(30, 70, 10), name = "Latitude") +
+
+ggplot() +
+  geom_raster(data = trendchange_dt , aes(x = X, y = Y, fill = `Frequency (%)`)) +
+  scale_fill_gradientn(colours =c('#3F52E3', '#F6F6F6','#F12D2D')) +
+  borders(colour = 'black', size = 0.8) +
+  coord_cartesian(xlim = c(-10,40), ylim = c(30, 45), expand = FALSE) +
+  labs(fill = "Relative Frequency (%)", title = "Frequency Maps", x = "Longitude", y = "Latitude") +
+  scale_x_continuous(labels = ~ paste0(.x, "°")) +
+  scale_y_continuous(labels = ~ paste0(.x, "°")) +
+  facet_wrap(~variable, nrow = 3, labeller = labeller(variable = c("pdsi_mn" = "PDSI", "spei_mn" = "SPEI", "tas_mn" = "Temperature (Centigrade)"))) +
   theme(panel.background = element_rect(fill = NA), panel.ontop = TRUE,
         axis.ticks.length = unit(0, "cm"),
         panel.grid.major = element_line(colour = "gray60"),
-        axis.title = element_text(size = 16), 
-        legend.text = element_text(size = 12), 
-        legend.title = element_text(size = 16))
+        panel.border = element_rect(size = 1, fill = NA),
+        panel.spacing =unit(1, "cm"), 
+        axis.title = element_blank(),
+        axis.text = element_text(size = 16),
+        plot.title = element_text(size = 20, hjust = 0.5),
+        legend.text = element_text(size = 16),
+        legend.key.width  = unit(4, "cm"),
+        legend.key.height = unit(1, "cm"),
+        legend.title = element_text(size = 20),
+        legend.position = 'bottom',
+        strip.background = element_rect(fill = NA), 
+        strip.text = element_text(size = 20, face = "bold"))
 
-ggsave(paste0(PATH_OUTFILES,"pdsi_frequncy_of_trend_change.png"), 
-       width = 12, height = 8)
+ggsave("Frequncy_maps.png", dpi = 600, height = 21, width = 16)
 
-
-ggplot(data = spei_trend_changes, aes(X, Y)) +
-  geom_tile(aes(fill =`Frequency (%)`, color=`Frequency (%)`)) +
-  scale_fill_gradient2(low = "blue",midpoint = 50,mid = "gray",   high = "red") +
-  scale_color_gradient2(low = "blue",midpoint =  50,mid = "gray",   high = "red") +
-  scale_x_continuous(breaks = seq(-20, 30, 10), name = "Longitude") +
-  scale_y_continuous(breaks = seq(30, 70, 10), name = "Latitude") +
-  theme(panel.background = element_rect(fill = NA), panel.ontop = TRUE,
-        axis.ticks.length = unit(0, "cm"),
-        panel.grid.major = element_line(colour = "gray60"),
-        axis.title = element_text(size = 16), 
-        legend.text = element_text(size = 12), 
-        legend.title = element_text(size = 16))
-
-ggsave(paste0(PATH_OUTFILES,"spei_frequncy_of_trend_change.png"), 
-       width = 12, height = 8)
-
-
-ggplot(data = tas_trend_changes, aes(X, Y)) +
-  geom_tile(aes(fill =`Frequency (%)`, color=`Frequency (%)`)) +
-  scale_fill_gradient2(low = "blue",midpoint = 50,mid = "gray",   high = "red") +
-  scale_color_gradient2(low = "blue",midpoint =  50,mid = "gray",   high = "red") +
-  scale_x_continuous(breaks = seq(-20, 30, 10), name = "Longitude") +
-  scale_y_continuous(breaks = seq(30, 70, 10), name = "Latitude") +
-  theme(panel.background = element_rect(fill = NA), panel.ontop = TRUE,
-        axis.ticks.length = unit(0, "cm"),
-        panel.grid.major = element_line(colour = "gray60"),
-        axis.title = element_text(size = 16), 
-        legend.text = element_text(size = 12), 
-        legend.title = element_text(size = 16))
-
-ggsave(paste0(PATH_OUTFILES,"tas_frequncy_of_trend_change.png"), 
-       width = 12, height = 8)
